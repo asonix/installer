7a04293d03769d1ee76fdb0c5b0937d711dedb23
Asonix <asonix.dev@gmail.com>
2014-03-03 21:06:01 -0600

    updating configs

--- switcher.py
+++ switcher.py
@@ -27,7 +27,7 @@ class ColorsObj(object):
 	
 	#location of background images
 	i3Lightbg = "/usr/share/backgrounds/16.jpg"	
-	i3Darkbg = "/usr/share/backgrounds/Space.png"
+	i3Darkbg = "/usr/share/backgrounds/direction.png"
 	i3Matrixbg = "~/Pictures/black.png"
 
 	#creation of variable for transparency
