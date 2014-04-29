4d030b1a2037a957bbf988fe62b134106353f453
asonix <asonix.dev@gmail.com>
2014-04-29 13:10:24 -0500

    updating configs

--- reread.sh
+++ reread.sh
@@ -3,6 +3,7 @@
 i3-msg restart
 xrdb ~/.Xresources
 
-gsettings set org.gnome.desktop.background picture-uri "file:////usr/share/backgrounds/164.jpg"
+gsettings set org.gnome.desktop.background draw-background false
+gsettings set org.gnome.desktop.background draw-background true
+gsettings set org.gnome.desktop.background picture-uri "file:////home/riley/Pictures/furfaggotry/katia_managan.jpg"
 
-# nohup ./alternating_layouts.py & exit
